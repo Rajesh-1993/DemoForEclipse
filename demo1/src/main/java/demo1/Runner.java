6e641229970e2bc741fb1855e7a9cf5885183026
user.email <rajesh.s@YB1012387LT.Yash.com>
2021-08-24 16:41:05 +0530

    initial commit

---
+++ demo1/src/main/java/demo1/Runner.java
@@ -0,0 +1,10 @@
+package demo1;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" Running....... very slow as of now");
+	}
+
+}
