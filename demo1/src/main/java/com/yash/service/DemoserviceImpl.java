e614a52d6c00998cfd5dc65bb0f5752de646df6b
user.email <rajesh.s@YB1012387LT.Yash.com>
2021-08-25 15:31:14 +0530

    commit without pull checking conflicts

--- demo1/src/main/java/com/yash/service/DemoserviceImpl.java
+++ demo1/src/main/java/com/yash/service/DemoserviceImpl.java
@@ -5,6 +5,7 @@ public class DemoserviceImpl implements demoService{
 	public int serviceToImplement() {
 		// TODO Auto-generated method stub
 		System.out.println("in impl");
+		System.out.println("no pull happen");
 		return 0;
 	}
 
